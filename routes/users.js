9b6f31d93acfce93c6aee89dffdbc976d66032a0
Nikita Konkin <niki-konkin@yandex.ru>
2022-05-10 01:48:55 +0300

    signs and users routes - created

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const {
+  celebrate,
+  Joi,
+} = require('celebrate');
+
+const {
+  getUserById,
+  updateUserProfile,
+} = require('../controllers/users');
+
+router.get('/profile/me', getUserById);
+router.patch('/profile/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    email: Joi.string().email().required(),
+    career: Joi.string().min(2).max(100),
+  }),
+}), updateUserProfile);
 
 module.exports = router;
